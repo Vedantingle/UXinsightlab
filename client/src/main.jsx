da09c31ac55d72e0bed388dc1ffcd24700beb3d1
Vedant <inglevedant43@gmail.com>
2026-04-15 15:49:00 +0530

    change the rules in the scaning

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,7 +5,6 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <h1>UX Analyzer – Analyze Your Website UX, SEO & Accessibility</h1>
     <App />
   </StrictMode>,
 )
